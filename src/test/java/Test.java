f24159faa55051b1c101c135b8ab009d3ca0d28b
kason-seu <zhangkaiseu@163.com>
2020-10-07 00:51:31 +0800

    stack to queue， queue to stack

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import common.ListNode;
+
+import java.util.LinkedList;
+
+public class Test {
+
+    public static void main(String[] args) {
+        LinkedList l = new LinkedList();
+        l.add(1);
+        l.add(2);
+        System.out.println(l.pop());
+    }
+}
