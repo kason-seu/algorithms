489cd85a9508271a235279c6d059dcf41a4c9728
kason-seu <zhangkaiseu@163.com>
2020-10-08 17:15:37 +0800

    cycle

--- src/main/java/common/NodeUtils.java
+++ src/main/java/common/NodeUtils.java
@@ -15,4 +15,19 @@ public class NodeUtils {
             node = node.next;
         }
     }
+
+    public static ListNode condtructList(int[] arr) {
+        ListNode head = null;
+        ListNode next = null;
+        for (int i = 0; i < arr.length; i++) {
+            if (head == null) {
+                head = new ListNode(arr[i]);
+                next = head;
+            } else {
+                next.next = new ListNode(arr[i]);
+                next = next.next;
+            }
+        }
+        return head;
+    }
 }
