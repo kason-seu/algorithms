eb7b0aa68f0d7d5de51d8a819a00a5840212f95a
kason-seu <zhangkaiseu@163.com>
2020-10-18 19:32:40 +0800

    获取无向图的联通分量明细

--- src/main/java/graph/GraphCC.java
+++ src/main/java/graph/GraphCC.java
@@ -1,32 +1,33 @@
 package graph;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import com.google.common.collect.Lists;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GraphCC {
 
     private Graph G;
-    private final boolean[] visited;
+    private final int[] visited;
     private int cccount;
 
     public GraphCC(Graph g) {
         this.G = g;
-        this.visited = new boolean[G.V()];
+        this.visited = new int[G.V()];
+        Arrays.fill(visited, -1);
         for (int v = 0; v < G.V(); v++) {
-            if (!visited[v]) {
-                cccount++;
-                dfs(v);
+            if (visited[v] == -1) {
+
+                dfs(v,cccount++);
             }
         }
     }
 
-    private void dfs(int v) {
-        visited[v] = true;
-
+    private void dfs(int v, int ccid) {
+        visited[v] = ccid;
         for (int w : G.adj(v)) {
-            if (!visited[w]) {
-                dfs(w);
+            if (visited[w] == -1) {
+                dfs(w,ccid);
             }
         }
 
@@ -36,11 +37,16 @@ public class GraphCC {
         return cccount;
     }
 
+    public int[] getVisited() {
+        return visited;
+    }
 
     public static void main(String[] args) {
         Graph graph = new Graph("g1.txt");
         GraphCC graphDFS = new GraphCC(graph);
 
         System.out.println(String.format("联通分量个数(connected component count) %d", graphDFS.getCccount()));
+        Arrays.stream(graphDFS.getVisited()).forEach(System.out::println);
+        System.out.println();
     }
 }
