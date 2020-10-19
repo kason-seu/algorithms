3fe3707ad86af98335870e6ac805ee99e17e290b
kason-seu <zhangkaiseu@163.com>
2020-10-19 22:30:49 +0800

    计算两点之间是否联通， 如果联通则减zhi

---
+++ src/main/java/graph/Path.java
@@ -0,0 +1,87 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 计算从srcId 到 targetId 是否存在路径
+ */
+public class Path {
+
+    private final Graph G;
+    private final int[] visited;
+    private int srcId;
+    private int targetId;
+    private final int[] pre; // 记录顶点的父亲定点
+
+    public Path(Graph g, int srcId, int targetId) {
+        this.G = g;
+        this.visited = new int[G.V()];
+        Arrays.fill(visited, -1);
+
+        pre = new int[G.V()];
+        Arrays.fill(pre, -1);
+
+        this.srcId = srcId;
+        this.targetId = targetId;
+        dfs(srcId, srcId);
+    }
+
+    private boolean dfs(int v, int parentId) {
+        visited[v] = 1;
+        pre[v] = parentId;
+        if (v == targetId) {
+            return true;
+        }
+        for (int w : G.adj(v)) {
+            if (visited[w] == -1) { // 没访问过
+                if (dfs(w,v) ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isConnected() {
+        G.validVertex(targetId);
+        Arrays.stream(visited).forEach(res -> {
+            System.out.print(res + " ");
+        });
+        System.out.println();
+        return visited[targetId] == 1;
+    }
+
+    public List<Integer> path() {
+        List<Integer> res = new ArrayList<>();
+        if (!isConnected()) {
+            return res;
+        }
+        while (targetId != srcId) {
+            res.add(targetId);
+            targetId = pre[targetId];
+        }
+        res.add(srcId);
+        Collections.reverse(res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph("g1.txt");
+        Path graphDFS = new Path(graph, 1, 4);
+
+        if (graphDFS.isConnected()) {
+            System.out.println("联通");
+        }
+        List<Integer> path = graphDFS.path();
+
+        path.stream().forEach(res -> {
+            System.out.print(res + " ");
+        });
+        System.out.println();
+
+
+    }
+}
