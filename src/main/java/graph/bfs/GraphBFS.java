508badb38ee6dc1a2371186ef30419abee00e944
kason-seu <zhangkaiseu@163.com>
2020-10-24 17:32:41 +0800

    广度优先遍历

---
+++ src/main/java/graph/bfs/GraphBFS.java
@@ -0,0 +1,61 @@
+package graph.bfs;
+
+import graph.Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class GraphBFS {
+
+    private Graph G;
+    private final boolean[] visited;
+    private LinkedList<Integer> queue;
+    private List<Integer> result = new ArrayList<>();
+
+    public GraphBFS(Graph g) {
+        this.G = g;
+        this.visited = new boolean[G.V()];
+        queue = new LinkedList<>();
+        for (int v = 0; v < G.V(); v++) {
+            if (!visited[v]) {
+                bfs(v);
+            }
+        }
+    }
+
+    private void bfs(int v) {
+        queue.addLast(v);
+        visited[v] = true;
+        while (!queue.isEmpty()) {
+            // 取出队列的头
+            Integer first = queue.removeFirst();
+            result.add(first);
+            if (first != null) {
+                for (int w : G.adj(first)) {
+                    if (!visited[w]) {
+                        queue.addLast(w);
+                        visited[w] = true;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    public List<Integer> getBfsResult() {
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Graph graph = new Graph("g1.txt");
+        GraphBFS graphBFS = new GraphBFS(graph);
+        graphBFS.getBfsResult().stream().forEach(res -> {
+            System.out.print(res + " ");
+        });
+        System.out.println();
+    }
+}
