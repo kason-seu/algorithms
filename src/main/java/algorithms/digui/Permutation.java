efc9b0608823c2b5d27b9f0fdddb816fa31ebaec
kason <zhangkaiseu@163.com>
2021-09-12 11:44:17 +0800

    递归简单题目

---
+++ src/main/java/algorithms/digui/Permutation.java
@@ -0,0 +1,73 @@
+package algorithms.digui;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 排列组合，去重和不去重的版本
+ */
+public class Permutation {
+
+
+    public static void main(String[] args) {
+        String input = "abc";
+        char[] inputArray = input.toCharArray();
+        List<String> result = new ArrayList<>();
+        permutation(inputArray, 0, result);
+        System.out.println(result);
+
+        input = "aabc";
+        char[] inputArrayRepeat = input.toCharArray();
+        List<String> resultNoRepeat = new ArrayList<>();
+        permutation(inputArrayRepeat, 0, resultNoRepeat);
+        System.out.println(resultNoRepeat);
+    }
+
+    /**
+     *
+     * @param origin 原始字符串对应的char数组
+     * @param index origin[0,..index-1] 已经确认好组合， orgin[i...] 从i开始的字符都可以在i的位置上
+     * @param ans: 收集符合条件的结果
+     */
+    static void permutation(char[] origin, int index, List<String> ans) {
+
+        if (index == origin.length) {
+            ans.add(String.valueOf(origin));
+        }
+        // index以及之后的索引都可以尝试成为index的位置
+        for( int j = index; j < origin.length; j++) {
+            swap(origin, index, j);
+            permutation(origin, index + 1, ans);
+            swap(origin, j, index);
+        }
+    }
+
+    private static void swap(char[] origin, int i, int j) {
+        char temp = origin[i];
+        origin[i]= origin[j];
+        origin[j] = temp;
+
+    }
+
+
+
+    static void permutationNoREpeat(char[] origin, int index, List<String> ans) {
+
+        if (index == origin.length) {
+            ans.add(String.valueOf(origin));
+        }
+        // index以及之后的索引都可以尝试成为index的位置
+        Set<Character> alreadyVisit = new HashSet<>();
+        for( int j = index; j < origin.length; j++) {
+            if (!alreadyVisit.contains(origin[index])) {
+                alreadyVisit.add(origin[index]);
+                swap(origin, index, j);
+                permutationNoREpeat(origin, index + 1, ans);
+                swap(origin, j, index);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/algorithms/digui/SubSequence.java
@@ -0,0 +1,63 @@
+package algorithms.digui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 字符串的全部子序列， 重复和不重复的子序列
+ */
+public class SubSequence {
+
+
+    public static void main(String[] args) {
+        String s = "abc";
+
+        List<String> subList = new ArrayList<>();
+        subSequence(s.toCharArray(), 0, subList, "");
+        System.out.println(subList);
+    }
+
+
+    /**
+     *
+     * @param strs 原始字符串
+     * @param index 该到哪个位置进行选择，要还是不要
+     * @param subList 已经走到了最后了，使用List记录所有沿途的路径
+     * @param path 沿途的路径
+     */
+    static void subSequence(char[] strs, int index, List<String> subList, String path) {
+
+        if (index == strs.length) {
+            subList.add(path);
+            return;
+        }
+
+        // 选择该index点的字符
+        String yes =  path + strs[index] ;
+        subSequence(strs, index + 1, subList, yes);
+
+        // 不选择该index点的字符
+        String no = path;
+
+        subSequence(strs, index+1, subList, no);
+    }
+
+
+    static void subSequenceNoRepeat(char[] strs, int index, Set<String> subList, String path) {
+
+        if (index == strs.length) {
+            subList.add(path);
+            return;
+        }
+
+        // 选择该index点的字符
+        String yes =  path + strs[index] ;
+        subSequenceNoRepeat(strs, index + 1, subList, yes);
+
+        // 不选择该index点的字符
+        String no = path;
+
+        subSequenceNoRepeat(strs, index+1, subList, no);
+    }
+}
