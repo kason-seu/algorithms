2e150d56013921cbc2e6b753b99c3d1dace58d5b
kason <kason@192.168.0.102>
2022-02-13 21:40:57 +0800

    workflow_v2_tag

--- workflow/src/main/java/com/kason/workflow/data/PeContext.java
+++ workflow/src/main/java/com/kason/workflow/data/PeContext.java
@@ -3,6 +3,9 @@ package com.kason.workflow.data;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * 串流程范围内的上下文
+ */
 public class PeContext {
     private Map<String, Object> info = new ConcurrentHashMap<>();
 
